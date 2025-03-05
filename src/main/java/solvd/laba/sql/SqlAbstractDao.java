d3ea2e08adf9c709084ce601f1f3dbe60eed7cf7
Tomasz Jelen <tomaszjelen0096@gmail.com>
2025-03-05 12:33:39 +0100

    Quick fix

--- src/main/java/solvd/laba/sql/SqlAbstractDao.java
+++ src/main/java/solvd/laba/sql/SqlAbstractDao.java
@@ -8,9 +8,9 @@ import java.sql.SQLException;
 
 public abstract class SqlAbstractDao {
     //TODO URL!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    private static String url = "jdbc:mysql://localhost:3306/Optimising logistics for order fulfilment";
-    private static String user = "user";
-    private static String password = "password";
+    private static String url = "";
+    private static String user = "";
+    private static String password = "";
     public static final ConnectionPool connectionPool = ConnectionPool.getInstance(5, url, user, password);
     Connection getConnection() throws SQLException, InterruptedException {
         return connectionPool.getConnection();
